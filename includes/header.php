c7710e7bd52778a9cad0ce1b40c829da309fc49a
Nurnabi Rahat <saturngoal@gmail.com>
2023-03-16 00:08:27 +0600

    profile info

--- includes/header.php
+++ includes/header.php
@@ -224,7 +224,8 @@
           <img class="userpicture" role="button" src="images1/users/player-1.png" alt="">
           <ul class="dropdown-menu end-0">
             <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><a class="dropdown-item" href="#">Add a new recipe</a></li>
+            <li><a class="dropdown-item" href="#">Saved recipes</a></li>
+            <li><a class="dropdown-item" href="#">Add a recipe</a></li>
             <li><a class="dropdown-item" href="#">My recipes</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Log out!</a></li>
